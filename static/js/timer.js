2352eba30d52dd7cb2578f2cecd5c7db36c2c189
shubham paliwal <61670651+shekhar17-sketch@users.noreply.github.com>
2022-04-19 21:03:48 +0530

    js timer

---
+++ static/js/timer.js
@@ -0,0 +1,46 @@
+let totalTime = 0;
+let timebeforPause = 0
+let Interval_Id;
+console.log("I'm in script file");
+function startTimer(){
+    let timer = document.getElementById("timer");
+    let inputTime = document.getElementById("timerInput");
+    let startTime = Date.now();
+    Interval_Id = setInterval(frame, 1000);
+    
+    function frame(){
+        try {
+        totalTime = timebeforPause + (Date.now() - startTime);
+        let mm = Math.floor(totalTime / (1000*60 ));
+        let ss = Math.floor( (totalTime/1000) % 60);
+        mm = mm.toString().padStart(2,'0');
+        ss = ss.toString().padStart(2,'0');
+        let timeString = `${mm}:${ss}`;
+        timer.innerHTML = timeString;
+        inputTime.value = timeString;
+            }
+        catch(err){
+        clearInterval(Interval_Id)
+        }
+    }
+}
+
+function pauseTimer(){
+    pausebutton = document.getElementById("pause")
+    console.log(pausebutton.value)
+    console.log(pausebutton.checked)
+    console.log(pausebutton)
+    if( pausebutton.checked )
+    {
+        clearInterval(Interval_Id);
+        timebeforPause = totalTime;
+        document.getElementById("sudokugrid").style.display = "none";
+        document.getElementById("gridReplacer").style.display = "block";
+    }
+    else{
+        document.getElementById("sudokugrid").style.display = "block";
+        document.getElementById("gridReplacer").style.display = "none";
+        startTimer()
+    }
+
+}
--- sudoku/settings.py
+++ sudoku/settings.py
@@ -143,6 +143,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = "/static/"
 django_heroku.settings(locals())
 
+STATICFILES_DIRS = [ os.path.join( BASE_DIR ,'static' )]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
